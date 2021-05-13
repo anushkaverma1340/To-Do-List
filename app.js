5f3996c6edafd3abbd5a9f966c038971231d1f0d
Anushka Verma <70652314+anushkaverma1340@users.noreply.github.com>
2021-05-13 16:59:58 +0530

    Updated main server file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 const app = express();
+const router = express.Router();
 
 let items = [];
 
@@ -12,53 +13,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/public', express.static(__dirname + '/public'));
 mongoose.connect("mongodb://localhost:27017/todoDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-const itemsSchema = new mongoose.Schema({ name: String });
-const Item = mongoose.model("Item", itemsSchema);
+const demo = require("./routes/demo.js");
+app.use(demo);
 
-const item1 = new Item({ name: "Welcome to To Do List app" });
-const item2 = new Item({ name: "Click '+' to add items" });
-const item3 = new Item({ name: "<-Click to remove items" });
+const root = require("./routes/root.js");
+app.use(root);
 
-const defaultItems = [item1, item2, item3];
+const del = require("./routes/delete.js");
+app.use(del);
 
-app.get("/", function(req, res) {
-  Item.find({},function(err,foundItems){
-    if(foundItems.length===0){
-      Item.insertMany(defaultItems, function(err) {
-        if (err) {
-          console.log(err);
-        }
-      });
-      res.redirect("/");
-    } else {
-      res.render("list", {
-        listName: "Today",
-        toDoItems: foundItems
-      });
-    }
-  });
-});
-
-app.get("/:listName", function(req, res) {
-  let listName = _.capitalize(req.params.listName);
-  res.render("list", {
-    listName: listName,
-    toDoItems: items
-  })
-});
-
-app.post("/", function(req, res) {
-  const newItem = new Item({name:req.body.newItem});
-  newItem.save();
-  res.redirect("/");
-});
-
-app.post("/delete", function(req, res) {
-  Item.findByIdAndRemove(req.body.checkbox, function(err,foundItem){
-    if(err){console.log(err);}
-  });
-  res.redirect("/");
-});
+const listname = require("./routes/listname.js");
+app.use(listname);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
